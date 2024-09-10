536ecb60855b9947225874e597067ef45486f458
jahanalem <s.r.alem19@gmail.com>
2024-09-10 13:26:19 +0200

    Write unit tests for ProductTypeController

--- API/ProductBrandControllerTests.cs
+++ API/ProductBrandControllerTests.cs
@@ -1,6 +1,4 @@
-﻿using AutoMapper;
-using Lili.Shop.API.Controllers;
-using Lili.Shop.API.Errors;
+﻿using Lili.Shop.API.Controllers;
 using Lili.Shop.DataAccess.Specifications;
 using Lili.Shop.Model.Entities;
 using Lili.Shop.Service.Helpers;
@@ -16,11 +14,43 @@ namespace Lili.Shop.API.Tests
     {
         private readonly Mock<IProductBrandService> _mockProductBrandService;
         private readonly ProductBrandController _controller;
+        private BrandSpecParams _validBrandParams;
+        private List<ProductBrand> _brands;
+        private Pagination<ProductBrand> _paginatedResult;
 
         public ProductBrandControllerTests()
         {
             _mockProductBrandService = new Mock<IProductBrandService>();
             _controller = new ProductBrandController(_mockProductBrandService.Object);
+
+            SetupSharedMockBehavior();
+        }
+
+        // Shared setup method for the mock and common data
+        private void SetupSharedMockBehavior()
+        {
+            _validBrandParams = new BrandSpecParams { PageIndex = 1, PageSize = 10 };
+
+            _brands = new List<ProductBrand>
+            {
+                new ProductBrand { Id = 1, Name = "Brand1", IsActive = true },
+                new ProductBrand { Id = 2, Name = "Brand2", IsActive = true }
+            };
+
+            _paginatedResult = new Pagination<ProductBrand>(
+                _validBrandParams.PageIndex,
+                _validBrandParams.PageSize,
+                _brands.Count,
+                _brands
+            );
+
+            // Mock for invalid parameters
+            _mockProductBrandService.Setup(service => service.GetPaginatedBrandsAsync(It.Is<BrandSpecParams>(p => p.PageIndex <= 0 || p.PageSize <= 0 && (p.IsActive == true || p.IsActive == false || p.IsActive == null))))
+                .ReturnsAsync(new FailureOperationResult<Pagination<ProductBrand>>(ErrorCode.InvalidData, "Invalid pagination parameters"));
+
+            // Mock for valid parameters
+            _mockProductBrandService.Setup(service => service.GetPaginatedBrandsAsync(It.Is<BrandSpecParams>(p => p.PageIndex > 0 && p.PageSize > 0 && (p.IsActive == true || p.IsActive == false))))
+                .ReturnsAsync(new SuccessOperationResult<Pagination<ProductBrand>>(_paginatedResult));
         }
 
         #region Test GetBrands
@@ -29,14 +59,7 @@ namespace Lili.Shop.API.Tests
         public async void GetBrands_ReturnsPaginatedResult_WhenBrandsExist()
         {
             // Arrange
-            var brandParams = new BrandSpecParams { PageIndex = 1, PageSize = 10 };
-            var brands = new List<ProductBrand> {
-                new ProductBrand { Id = 1, Name = "Brand1", IsActive = true },
-                new ProductBrand { Id = 2, Name = "Brand2", IsActive = true }
-            };
-            var paginatedResult = new Pagination<ProductBrand>(brandParams.PageIndex, brandParams.PageSize, brands.Count, brands);
-            _mockProductBrandService.Setup(service => service.GetPaginatedBrandsAsync(brandParams))
-                .ReturnsAsync(new SuccessOperationResult<Pagination<ProductBrand>>(paginatedResult));
+            var brandParams = new BrandSpecParams { PageIndex = 1, PageSize = 10, IsActive = true };
 
             // Act
             var result = await _controller.GetBrands(brandParams);
@@ -68,6 +91,22 @@ namespace Lili.Shop.API.Tests
             var actionResult = Assert.IsType<ActionResult<Pagination<ProductBrand>>>(result);
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
             Assert.Equal(400, badRequestResult.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(1, 10)]
+        public async void GetBrands_ReturnsOk_WhenParamsAreValid(int pageIndex, int pageSize)
+        {
+            // Arrange
+            var validBrandParams = new BrandSpecParams { PageIndex = pageIndex, PageSize = pageSize, IsActive = true };
+
+            // Act
+            var result = await _controller.GetBrands(validBrandParams);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<Pagination<ProductBrand>>>(result);
+            var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            Assert.Equal(200, okResult.StatusCode);
         }
 
         #endregion
---
+++ API/ProductTypeControllerTests.cs
@@ -0,0 +1,178 @@
+﻿using Lili.Shop.API.Controllers;
+using Lili.Shop.DataAccess.Specifications.Params;
+using Lili.Shop.Model.Entities;
+using Lili.Shop.Service.Helpers;
+using Lili.Shop.Service.Helpers.OperationResults;
+using Lili.Shop.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Lili.Shop.Tests.API
+{
+    public class ProductTypeControllerTests
+    {
+        private readonly Mock<IProductTypeService> _mockProductTypeService;
+        private readonly ProductTypeController _controller;
+
+        private ProductTypeSpecParams _validTypeParams;
+        private List<ProductType> _types;
+        private Pagination<ProductType> _paginatedResult;
+
+        public ProductTypeControllerTests()
+        {
+            _mockProductTypeService = new Mock<IProductTypeService>();
+            _controller = new ProductTypeController(_mockProductTypeService.Object);
+
+            SetupSharedMockBehavior();
+        }
+
+        // Shared setup method for the mock and common data
+        private void SetupSharedMockBehavior()
+        {
+            _validTypeParams = new ProductTypeSpecParams { PageIndex = 1, PageSize = 10 };
+
+            _types = new List<ProductType>
+            {
+                new ProductType { Id = 1, Name = "Type1", IsActive = true },
+                new ProductType { Id = 2, Name = "Type2", IsActive = true }
+            };
+
+            _paginatedResult = new Pagination<ProductType>(
+                _validTypeParams.PageIndex,
+                _validTypeParams.PageSize,
+                _types.Count,
+                _types
+            );
+
+            // Mock for invalid parameters
+            _mockProductTypeService.Setup(service => service.GetPaginatedProductTypesAsync(It.Is<ProductTypeSpecParams>(p => p.PageIndex <= 0 || p.PageSize <= 0)))
+                .ReturnsAsync(new FailureOperationResult<Pagination<ProductType>>(ErrorCode.InvalidData, "Invalid pagination parameters"));
+
+            // Mock for valid parameters
+            _mockProductTypeService.Setup(service => service.GetPaginatedProductTypesAsync(It.Is<ProductTypeSpecParams>(p => p.PageIndex > 0 && p.PageSize > 0)))
+                .ReturnsAsync(new SuccessOperationResult<Pagination<ProductType>>(_paginatedResult));
+        }
+
+        #region Test GetProductTypes
+
+        [Fact]
+        public async void GetTypes_ReturnsPaginatedResult_WhenTypesExist()
+        {
+            // Arrange
+            var productTypeSpecParams = new ProductTypeSpecParams { PageIndex = 1, PageSize = 10 };
+
+
+            // Act
+            var result = await _controller.GetTypes(productTypeSpecParams);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<Pagination<ProductType>>>(result);
+            var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            Assert.Equal(200, okResult.StatusCode);
+
+            var returnedProductTypes = Assert.IsType<Pagination<ProductType>>(okResult.Value);
+            Assert.Equal(2, returnedProductTypes.Count);
+            Assert.Equal(returnedProductTypes.PageSize, returnedProductTypes.PageSize);
+            Assert.Equal(returnedProductTypes.PageIndex, returnedProductTypes.PageIndex);
+        }
+
+        [Theory]
+        [InlineData(-1, 10)]
+        [InlineData(1, -10)]
+        [InlineData(0, 10)]
+        [InlineData(1, 0)]
+        public async void GetTypes__ReturnsBadRequest_WhenParamsAreInvalid(int pageIndex, int pageSize)
+        {
+            // Arrange
+            var invalidParams = new ProductTypeSpecParams { PageIndex = pageIndex, PageSize = pageSize, IsActive = true };
+
+            // Resutl
+            var result = await _controller.GetTypes(invalidParams);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<Pagination<ProductType>>>(result);
+            var badRequestObjectResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+            Assert.Equal(400, badRequestObjectResult.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(1, 10)]
+        public async void GetTypes__ReturnsOk_WhenParamsAreValid(int pageIndex, int pageSize)
+        {
+            // Arrange
+            var validParams = new ProductTypeSpecParams { PageIndex = pageIndex, PageSize = pageSize };
+
+            // Act
+            var result = await _controller.GetTypes(validParams);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<Pagination<ProductType>>>(result);
+            var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+
+        #endregion
+
+        #region GetProductTypeById
+
+        [Fact]
+        public async void GetProductTypeById__ReturnsNotFound_WhenProductTypeDoesNotExist()
+        {
+            // Arrange
+            int productTypeId = 1000;
+            _mockProductTypeService.Setup(service => service.GetTypeByIdAsync(productTypeId))
+                .ReturnsAsync(new FailureOperationResult<ProductType>(ErrorCode.ResourceNotFound, "Product Type not found"));
+
+            // Act
+            var result = await _controller.GetProductTypeById(productTypeId);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<ProductType>>(result);
+            var objectResult = Assert.IsType<ObjectResult>(actionResult.Result);
+            Assert.Equal(404, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public async void GetProductTypeById__ReturnsProductType_WhenTypeExists()
+        {
+            // Arrange
+            var productTypeId = 1;
+            var expectedProductType = new ProductType
+            {
+                Id = 1,
+                Name = "Type1",
+                IsActive = true,
+            };
+            _mockProductTypeService.Setup(service => service.GetTypeByIdAsync(productTypeId))
+                .ReturnsAsync(new SuccessOperationResult<ProductType>(expectedProductType));
+
+            // Act
+            var result = await _controller.GetProductTypeById(productTypeId);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<ProductType>>(result);
+            var objectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            Assert.Equal(200, objectResult.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(-2)]
+        [InlineData(null)]
+        public async void GetProductTypeById__ReturnsBadRequest_WhenParamsAreInvalid(int productTypeId)
+        {
+            // Arrange
+            _mockProductTypeService.Setup(service => service.GetTypeByIdAsync(productTypeId))
+                .ReturnsAsync(new FailureOperationResult<ProductType>(ErrorCode.InvalidData));
+
+            // Act
+            var result = await _controller.GetProductTypeById(productTypeId);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<ProductType>>(result);
+            var badRequestObjectResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+            Assert.Equal(400, badRequestObjectResult.StatusCode);
+        }
+
+        #endregion
+    }
+}
